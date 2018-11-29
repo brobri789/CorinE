3aa34849a6fe4510dba9f6ba426f32e2abe064f0
KDC <KDC@KDC>
2018-11-29 17:37:34 +0900

    asdsad

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,20 +1,21 @@
-package org.zerock.controller;
+package org.zerock.controller; 
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 
-import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
@@ -101,9 +102,38 @@ public class SampleController
 		log.info("/ex06...........");
 		SampleDTO dto = new SampleDTO();
 		dto.setAge(10);
-		dto.setName("ȫ�浿");
+		dto.setName("�솉湲몃룞");
 		return dto;
 	}
 	
+	
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07()
+	{
+		log.info("/ex07...........");
+		String msg = "{\"name \" : \"홍길동\"}";
+		
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-type", "application/json;charset = UTF-8");
+		
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/exUpload")
+	public void exUpload()
+	{
+		log.info("exUpload...............");
+	}
+	
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files)
+	{
+		files.forEach(file -> {
+		log.info("--------------------");
+		log.info("name" + file.getOriginalFilename());
+		log.info("size" + file.getSize());
+	});
+	}
 	
 }
